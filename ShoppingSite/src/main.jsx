10db61dd51ed83a3cb28ec39b44d2ddb8092ca70
Abhishek Surse <abhisheksurse5@gmail.com>
2024-11-21 18:11:10 +0530

    Added Home item and loading functionality in it.

--- ShoppingSite/src/main.jsx
+++ ShoppingSite/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./routes/App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Bag from "./routes/Bag.jsx";
 import Home from "./routes/Home.jsx";
--- ShoppingSite/src/routes/App.jsx
+++ ShoppingSite/src/routes/App.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import HomeItem from "../components/HomeItem";
+import FetchItems from "../components/FetchItems";
+import LoadingSpinner from "../components/LoadingSpinner";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fetchStatus = useSelector((store) => store.fetchStatus);
   return (
     <>
       <Header />
-      <Outlet />
+      <FetchItems />
+      {fetchStatus.currentlyFetching ? <LoadingSpinner /> : <Outlet />}
       <Footer />
     </>
   );
